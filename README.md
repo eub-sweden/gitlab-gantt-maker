c5ad310f132e4f80595b597b91700467188fa38b
Magnus Axelsson <magnus@eub.se>
2022-11-04 18:58:15 +0100

    Commit first version.

---
+++ README.md
@@ -0,0 +1,23 @@
+# gitlab-gantt-maker
+
+## Backgruound
+Many simpler projects and smaller teams that use Gitlab for source code management do just fine with the Free edition although it lacks some advanced project management features. This project was created out of a need to visualize project timelines, which was the one and only non-free feature needed.
+
+## What it does
+The 'gitlab-gantt-maker' script uses the Gitlab API to list milestones and related issues, visualizing them in the form of a Gantt chart. Group milestones are placed on top, considered very high-level timeline items such as project completion. For each project in the group, project timelines are listed next, with their associated issues right under them. Each bar in the Gantt chart has a clickable hyperlink which opens the Gitlab milestone or issue.
+
+![Screenshot](res/screenshot.png)
+
+## How to use
+First install the Python dependencies:
+`pip install -r requirements.txt`
+
+Then edit the configuration file `config.ini`.
+* Instance is the URL for your Gitlab instance, for projects hosted at Gitlab.com just leave as is.
+* Group is the Gitlab group for which you want to generate a Gantt chart. It even works setting the name of a subgroups, at least as long as it has a unique name.
+* PersonalAccessToken must be a Gitlab personal access token with API scope. See https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html for information on how to create a token.
+
+Finally the script can be executed with:
+`python gitlab-gantt-maker.py`
+
+With no options, `config.ini` will be used as the configuration file and the output will be `gantt.html`. Execute the script with `python gitlab-gantt-maker.py` for help on available parameters.
---
+++ gitlab-gantt-maker.py
@@ -0,0 +1,178 @@
+import gitlab
+import plotly.express as px
+import plotly.io as pio
+import pandas as pd
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import configparser
+import math
+import argparse
+
+
+def datestr(date):
+    return date.strftime("%Y-%m-%d")
+
+
+def strip_tz(datestr):
+    return datestr[0:10]
+
+
+def datestr_add_a_day(date):
+    d = datetime.fromisoformat(strip_tz(date))
+    d += timedelta(days=1)
+    return d.strftime("%Y-%m-%d")
+
+
+# If a milestone does not have a due date,
+# one year from now is assumed
+end_of_time = datetime.now()
+end_of_time += relativedelta(years=1)
+end_of_time = datestr(end_of_time)
+
+
+class GanttMaker:
+    def __init__(self, filename, verbose=False):
+        self.filename = filename
+        self.tasks = []
+        self.idx = 0
+        self.colormap = {
+            "Group Milestone": "green",
+            "Project Milestone": "blue",
+            "Issue": "goldenrod",
+        }
+
+    def __repr__(self):
+        return str(pd.DataFrame(self.tasks))
+
+    def add_task(self, name, start, finish, url, type="Issue"):
+        hyperlink = f"""<a href="{url}", target="_black">o</a>"""
+        self.tasks.append(
+            dict(
+                Index=self.idx,
+                Task=name,
+                Start=start,
+                Finish=finish,
+                Url=hyperlink,
+                Resource=type,
+            )
+        )
+        self.idx += 1
+
+    def _annotate(self):
+        for idx, row in self.df.iterrows():
+            periods = pd.date_range(row["Start"], row["Finish"], freq="1D")
+            center_pos = math.floor(len(periods) / 2)
+            x_dates = periods[center_pos]
+            self.fig.add_annotation(
+                {
+                    "x": x_dates,  # row["Finish"],
+                    "y": row["Task"],
+                    "text": row["Url"],
+                    "align": "center",
+                    "showarrow": False,
+                }
+            )
+
+    def write(self):
+        self.df = pd.DataFrame(self.tasks)
+        self.fig = px.timeline(
+            self.df,
+            x_start="Start",
+            x_end="Finish",
+            y="Task",
+        )
+        self.fig.update_traces(
+            marker_color=[self.colormap[r] for r in self.df.Resource]
+        )
+        self.fig.update_yaxes(autorange="reversed")
+        self._annotate()
+        with open(self.filename, "w") as f:
+            f.write(pio.to_html(self.fig))
+
+
+def extract_milestone(ms):
+    start_date = ms.created_at if not ms.start_date else ms.start_date
+    due_date = ms.due_date if ms.due_date else end_of_time
+    return ms.title, strip_tz(start_date), strip_tz(due_date), ms.web_url
+
+
+def extract_issue(i, start_date, due_date):
+    start = start_date if i.created_at < start_date else i.created_at
+    due = i.due_date if i.due_date else datestr_add_a_day(start)
+    return i.title, strip_tz(start), strip_tz(due), i.web_url
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog="gitlab-gantt-maker",
+        description="Makes a simple Gantt chart out of Gitlab milestones as a standalone HTML file using the Gitlab API",
+    )
+    parser.add_argument(
+        "-c", "--config", help="configuration file path", type=str, default="config.ini"
+    )
+    parser.add_argument(
+        "-o", "--output", help="HTML output file path", type=str, default="gantt.html"
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")  # on/off flag
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    try:
+        cfg_token = config["gitlab"]["PersonalAccessToken"]
+        cfg_inst = config["gitlab"]["Instance"]
+        cfg_group = config["gitlab"]["Group"]
+    except KeyError:
+        print("Missing or incomplete configuration file")
+        exit(1)
+
+    # private token or personal token authentication (GitLab.com)
+    gl = gitlab.Gitlab(cfg_inst, private_token=cfg_token)
+
+    # make an API request to create the gl.user object. This is not required but may be useful
+    # to validate your token authentication. Note that this will not work with job tokens.
+    gl.auth()
+
+    # Enable "debug" mode. This can be useful when trying to determine what
+    # information is being sent back and forth to the GitLab server.
+    # Note: this will cause credentials and other potentially sensitive
+    # information to be printed to the terminal.
+    # gl.enable_debug()
+
+    g = gl.groups.list(search=cfg_group)
+
+    if not g:
+        print("Group not found or API permissions missing")
+        exit(1)
+
+    gc = GanttMaker(args.output)
+
+    group = g[0]
+    projlist = group.projects.list()
+
+    # Group milestones
+    for groupms in group.milestones.list():
+        gm = group.milestones.get(groupms.id)
+        title, start_date, due_date, url = extract_milestone(gm)
+        gc.add_task(title, start_date, due_date, url, "Group Milestone")
+
+    # Project milestones
+    for proj in projlist:
+        p = gl.projects.get(proj.id)
+        for pm in p.milestones.list():
+            title, start_date, due_date, url = extract_milestone(pm)
+            gc.add_task(title, start_date, due_date, url, "Project Milestone")
+            for i in pm.issues():
+                ititle, istart_date, idue_date, url = extract_issue(
+                    i, start_date, due_date
+                )
+                gc.add_task(ititle, istart_date, idue_date, url)
+
+    if args.verbose:
+        print(gc)
+    gc.write()
+
+
+if __name__ == "__main__":
+    main()
